a901b04f7adabfc036474b86d6316d71002a9cc8
Jonatan Ekström <jonatan.ekstrom90@gmail.com>
2014-10-27 19:46:51 +0100

    Add class implementing the IMU interface.

---
+++ sensor/src/classes/alt_imu.cc
@@ -0,0 +1,46 @@
+/*
+* This file defines the AltIMU class which models the specific IMU used in
+* the project. It uses an I2CHandler factory to initialize its I2CHandlers.
+*/
+
+#include "alt_imu.h"
+#include "../interfaces/i2c_handler_factory.h"
+#include "../interfaces/i2chandler.h"
+#include <vector>
+
+AltIMU::AltIMU(const I2CHandlerFactory *const handlerFactory)
+{
+	accAndMagHandler = handlerFactory->MakeAccAndMagHandler();
+	gyroscopeHandler = handlerFactory->MakeGyroscopeHandler();
+	barometerHandler = handlerFactory->MakeBarometerHandler();
+}
+
+AltIMU::~AltIMU()
+{
+	delete accAndMagHandler;
+	delete gyroscopeHandler;
+	delete barometerHandler;
+}
+
+std::vector<const Measurement*> AltIMU::GetNextMeasurementBatch() const
+{
+	std::vector<const Measurement*> measurementBatch;
+
+	/* For testing purposes only */
+	measurementBatch.push_back(accAndMagHandler->GetNextMeasurement());
+	measurementBatch.push_back(accAndMagHandler->GetNextMeasurement());
+	measurementBatch.push_back(gyroscopeHandler->GetNextMeasurement());
+	measurementBatch.push_back(barometerHandler->GetNextMeasurement());
+	measurementBatch.push_back(barometerHandler->GetNextMeasurement());
+
+	return measurementBatch;
+}
+
+void AltIMU::GetAllAvailableMeasurementsFromHandler(std::vector<const Measurement*>& measurementBatch,
+													const I2CHandler *const handler) const
+{
+	while (handler->HasAvailableMeasurements())
+	{
+		measurementBatch.push_back(handler->GetNextMeasurement());
+	}
+}
---
+++ sensor/src/classes/alt_imu.h
@@ -0,0 +1,33 @@
+/*
+* This file declares the AltIMU class which models the specific IMU used in
+* the project. It uses an I2CHandler factory to initialize its I2CHandlers.
+*/
+
+#ifndef ALT_IMU_H_
+#define ALT_IMU_H_
+
+#include "../interfaces/imu.h"
+#include <vector>
+
+class I2CHandlerFactory;
+class I2CHandler;
+class Measurement;
+
+class AltIMU : public IMU
+{
+public:
+	explicit AltIMU(const I2CHandlerFactory *const handlerFactory);
+	virtual ~AltIMU();
+	virtual std::vector<const Measurement*> GetNextMeasurementBatch() const;
+private:
+	AltIMU(const AltIMU&);
+	AltIMU& operator=(const AltIMU&);
+
+	void GetAllAvailableMeasurementsFromHandler(std::vector<const Measurement*>& measurementBatch,
+												const I2CHandler *const handler) const;
+	I2CHandler* accAndMagHandler;
+	I2CHandler* gyroscopeHandler;
+	I2CHandler* barometerHandler;
+};
+
+#endif
