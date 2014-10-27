7468a77abb134b0de0560767667b954c802c99e9
Johan Norberg <norberg_johan@hotmail.com>
2014-10-27 22:03:23 +0100

    Add basic premake4 project

---
+++ sensor/src/main.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "classes/i2chandlers/default_handler_factory.h"
+
+int main()
+{
+	std::cout << "Hello world!" << std::endl;
+
+	return 0;
+}
---
+++ sensor/src/premake4.lua
@@ -0,0 +1,20 @@
+solution "Sensor"
+   configurations { "Debug", "Release" }
+   location "build"
+
+   project "Sensor"
+      kind "ConsoleApp"
+      language "C++"
+      
+      files { "**.h", "**.cc"}
+
+      configuration "Debug"
+         defines { "DEBUG" }
+         flags { "Symbols" }
+         targetdir "bin/debug"
+
+      configuration "Release"
+         defines { "NDEBUG" }
+         flags { "Optimize" }
+         targetdir "bin/release"  
+
