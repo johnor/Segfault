e6c9227f0ed9a72d7e0c9fb43a0f946e8c5fbf03
Johan Norberg <norberg_johan@hotmail.com>
2014-11-01 19:37:20 +0100

    Add basic logger

---
+++ sensor/src/classes/Logger.cc
@@ -0,0 +1,91 @@
+#include "Logger.h"
+#include <iostream>
+
+/* Logger implementation */
+void Logger::Log(const std::string &message)
+{
+	auto logger = Logger::GetInstance();
+
+	logger->fs << message << std::endl;
+	std::cout << message << std::endl;
+}
+
+void Logger::Log(const LogLevel level, const std::string &message)
+{
+	std::stringstream fullMessage;
+	fullMessage << "[" << getStringFromLogLevel(level) << "] ";
+	fullMessage << message;
+
+	Logger::Log(fullMessage.str());
+}
+
+LogStream Logger::Log()
+{
+	auto logger = Logger::GetInstance();
+	return LogStream(*logger, LogLevel::Info);
+}
+
+LogStream Logger::Log(const LogLevel level)
+{
+	auto logger = Logger::GetInstance();
+	return LogStream(*logger, level);
+}
+
+/* Private function implementations */
+
+Logger::Logger()
+{
+	fs.open(filename);
+	fs << "Logger created!" << std::endl;
+}
+
+std::shared_ptr<Logger>& Logger::GetInstance()
+{
+	static std::shared_ptr<Logger> instance = nullptr;
+
+	if (instance == nullptr)
+	{
+		instance = std::shared_ptr<Logger>{new Logger};
+	}
+	return instance;
+}
+
+std::string Logger::getStringFromLogLevel(const LogLevel level)
+{
+	std::string res;
+
+	switch (level)
+	{
+	case LogLevel::Debug:
+		res = "Debug";
+		break;
+	case LogLevel::Error:
+		res = "Error";
+		break;
+	case LogLevel::Info:
+		res = "Info";
+		break;
+	case LogLevel::Warning:
+		res = "Warning";
+		break;
+	default:
+		return "";
+	}
+	return res;
+}
+
+
+LogStream::LogStream(Logger& logger_, LogLevel level_) :
+	logger(logger), level(level_)
+{
+}
+
+LogStream::LogStream(const LogStream& ls) :
+	logger(ls.logger), level(ls.level)
+{
+}
+
+LogStream::~LogStream()
+{
+	logger.Log(level, str());
+}
---
+++ sensor/src/classes/Logger.h
@@ -0,0 +1,56 @@
+
+#ifndef LOG_H_
+#define LOG_H_
+
+#include <fstream>
+#include <sstream>
+#include <memory>
+#include <string>
+
+enum class LogLevel
+{
+	Debug = 0,
+	Info,
+	Warning,
+	Error
+};
+
+class Logger;
+
+class LogStream : public std::ostringstream
+{
+public:
+	LogStream(Logger& oLogger, LogLevel level);
+	LogStream(const LogStream& ls);
+	~LogStream();
+
+private:
+	Logger &logger;
+	LogLevel level;
+};
+
+class Logger
+{
+public:
+	~Logger() = default;
+
+	static void Log(const std::string &message);
+	static void Log(const LogLevel level, const std::string &message);
+
+	static LogStream Log();
+	static LogStream Log(LogLevel level);
+
+private:
+	Logger();
+	Logger(const Logger&) = delete;
+	Logger& operator=(const Logger&) = delete;
+
+	static std::shared_ptr<Logger>& GetInstance();
+	static std::string getStringFromLogLevel(const LogLevel level);
+
+	std::string filename{ "log.txt" };
+	std::ofstream fs;
+};
+
+
+#endif
