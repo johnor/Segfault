1bcac4189c9868826bd76a5d1e5440b4cf1ff855
Johan Norberg <norberg_johan@hotmail.com>
2015-01-25 13:05:43 +0100

    Use more threads when compiling and add switch to launch visual studio

--- sensor/build_rpi.sh
+++ sensor/build_rpi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash      
 
 tools/premake4 --platform=rpi gmake
-make -C build config=debug_rpi verbose=false
+make -C build config=debug_rpi verbose=false -j4
--- sensor/generate_vs2013.sh
+++ sensor/generate_vs2013.sh
@@ -1,3 +1,8 @@
 #!/bin/bash      
 
 tools/premake4 vs2013
+
+if [ $1 = "--open" ] 
+then :
+   start build/Sensor.sln
+fi
